f612dfe6048bc3f2c926bf958eebea01a0fde71e
EugeneL01 <eugene@webwiser.io>
2024-04-18 16:21:13 +0200

    update

--- src/store/api/search-data/index.ts
+++ src/store/api/search-data/index.ts
@@ -9,18 +9,16 @@
 //   return searchData;
 // };
 
-
 export const fetchSearchResults = async (term: any) => {
   try {
     let response = await fetch(
-      `https://itunes.apple.com/search?term=${term+'&media=music'}`
+      `https://itunes.apple.com/search?term=${term + "&media=music"}`
     );
 
     const data = await response.json();
     // setSearchData(data.results);
 
     console.log(data);
-    
 
     return data.results;
   } catch (error) {
