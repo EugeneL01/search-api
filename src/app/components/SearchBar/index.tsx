1d513beb8a2d7b732d3c7533978ac32c19d43b2c
EugeneL01 <eugene@webwiser.io>
2024-04-18 15:08:40 +0200

    update

--- src/app/components/SearchBar/index.tsx
+++ src/app/components/SearchBar/index.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 const SearchBar = () => {
   const [searchInputValue, setSearchInputValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [toggleSearch, setToggleSearch] = useState(false)
+  const [toggleSearch, setToggleSearch] = useState(false);
 
   const handleSearchValue = (event: any) => {
     if (event.key === "Enter") {
@@ -30,9 +30,9 @@ const SearchBar = () => {
   return (
     <>
       <div className="flex flex-col">
-        <div className="search-box">
+        <div className={`search-box ${toggleSearch ? "" : ""}`}>
           <input
-            className={`search-text ${toggleSearch ? 'w-[240px]' : 'w-0'}`}
+            className={`${toggleSearch ? "w-[245px] px-[10px]" : "w-0"} search-text ease-in-out duration-500`}
             type="text"
             id="inputText"
             placeholder="Type something..."
@@ -40,13 +40,31 @@ const SearchBar = () => {
             onChange={handleSearchValue}
             onKeyDown={handleSearchValue}
           />
-          <div onClick={() => {setToggleSearch(true)}} className={`search-btn ${toggleSearch ? 'bg-white text-black' : 'bg-[#2f3640] text-white'}`}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6`}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          <div
+            onClick={() => {
+              setToggleSearch(!toggleSearch);
+            }}
+            className={`search-btn ease-in-out duration-[1s] ${
+              toggleSearch ? "bg-white text-black" : "bg-[#2f3640] text-white"
+            }`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={`w-6 h-6`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
             </svg>
           </div>
         </div>
-        <div className="flex flex-row flex-wrap gap-[20px]">
+        <div className="flex flex-row flex-wrap gap-[20px] pt-[20px]">
           {searchResults?.map((result: any, index: number) => {
             return (
               <div key={index} className="max-w-[100px]">
